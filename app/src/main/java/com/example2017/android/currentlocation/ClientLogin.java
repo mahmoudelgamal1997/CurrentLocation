26227decbe05bbf9b337b672e18f2ed99ed2719a
mahmoudelgamal1997 <mahmoudelgamal1997@gmail.com>
2018-08-30 05:12:52 +0200

    login

--- app/src/main/java/com/example2017/android/currentlocation/ClientLogin.java
+++ app/src/main/java/com/example2017/android/currentlocation/ClientLogin.java
@@ -1,11 +1,17 @@
 package com.example2017.android.currentlocation;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class ClientLogin extends AppCompatActivity {
@@ -29,13 +35,55 @@ public class ClientLogin extends AppCompatActivity {
         login=(Button) findViewById(R.id.but_login);
 
 
+      firebaseAuthListener =new FirebaseAuth.AuthStateListener() {
+          @Override
+          public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+              if (firebaseAuth!=null)
+              {
+                  Intent intent=new Intent(ClientLogin.this,ClientMap.class);
+                  startActivity(intent);
+                  finish();
+                  return;
+              }
+          }
+      };
+
+
+
+
         login.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                
+            public void onClick(View view)
+            {
+                firebaseAuth.signInWithEmailAndPassword(email.getText().toString(),password.getText().toString()).addOnCompleteListener(ClientLogin.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task)
+                    {
+                        Intent intent=new Intent(ClientLogin.this,ClientMap.class);
+                        startActivity(intent);
+                        finish();
+
+                        Toast.makeText(ClientLogin.this, "done", Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
         });
 
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseAuth.addAuthStateListener(firebaseAuthListener);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+       firebaseAuth.addAuthStateListener(firebaseAuthListener);
 
     }
 }
